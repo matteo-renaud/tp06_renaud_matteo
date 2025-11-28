d658d37846a334db149e0e4c896e5197c0aa778e
Mattéo RENAUD <mrenaud2@cfai-formation.fr>
2025-11-28 13:04:11 +0100

    chore: fichier pour déploiement sur render

--- api/config.js
+++ api/config.js
@@ -1,10 +1,10 @@
 module.exports =  {
     ACCESS_TOKEN_SECRET : "EMMA123",
     BDD : {
-    "host" :"dpg-d41mvbq4d50c73eto1ag-a.oregon-postgres.render.com",
+    "host" :"dpg-d4i4dhqli9vc73ei93lg-a.oregon-postgres.render.com",
     "port" : "5432",
-    "user" : "pollution_mzxu_user",
-    "password" : "3dIzkrm8lYw7YoPupodOFbfjus6lESrF",
-    "bdname" :"pollution_mzxu"
+    "user" : "pollution_s76s_user",
+    "password" : "YGMtmxnrQMP77DNXvIY6wOmuIx2ZCGoy",
+    "bdname" :"pollution_s76s"
     }
 }
